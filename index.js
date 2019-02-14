c9551e597b7eed2b7e45821554648847cba13628
Diogo Costa <dsgmcosta@gmail.com>
2019-02-14 16:23:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
-  
+
   var result = "The line is currently: "
   for (let i = 0; i < line.length; i++) {
     if (i < line.length - 1) {
